a43fdb6dc7c4be9f6838ae7528459dd9cfde24cd
AlauddinTheSufiBhai <mdalauddin8948@gmail.com>
2022-12-05 15:57:44 +0600

    erwt

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
+import Home from "./components/Home";
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>This is header</Text>
+      <Home />
       <StatusBar style="auto" />
     </View>
   );
@@ -12,7 +14,7 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    // flex: 1,
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
